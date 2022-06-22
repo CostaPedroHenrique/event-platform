16127af163d803920983d249b91eb82b9a8ec77b
Pedro Costa <devpedrohenrique@gmail.com>
2022-06-21 23:49:20 -0300

    style: formated css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-    extends: [
-        'prettier',
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:import/typescript',
-        'plugin:jsx-a11y/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-        jsx: true
-        },
-        ecmaVersion: 11,
-        sourceType: 'module',
-        extraFileExtensions: ['.mjs']
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    rules: {
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "typescript-eslint/consistent-type-definitions": "off"
-    }
+    ecmaVersion: 11,
+    sourceType: 'module',
+    extraFileExtensions: ['.mjs']
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off'
+  }
 }
