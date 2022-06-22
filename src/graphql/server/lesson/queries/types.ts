fd30d1bb97ec3dc28f335917eba41a96fa9efa70
Pedro Costa <devpedrohenrique@gmail.com>
2022-06-22 20:44:00 -0300

    refactor: adjusted video component

--- src/graphql/server/lesson/queries/types.ts
+++ src/graphql/server/lesson/queries/types.ts
@@ -8,4 +8,25 @@ interface IGetLessonsResponse {
   }[]
 }
 
-export type { IGetLessonsResponse }
+interface ITeacher {
+  name: string
+  bio: string
+  avatarURL: string
+}
+interface IGetLessonBySlugVariables {
+  slug: string
+}
+interface IGetLessonBySlugResponse {
+  lesson: {
+    title: string
+    videoId: string
+    description: string
+    teacher: ITeacher
+  }
+}
+
+export type {
+  IGetLessonsResponse,
+  IGetLessonBySlugResponse,
+  IGetLessonBySlugVariables
+}
